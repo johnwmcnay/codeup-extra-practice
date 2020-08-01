56f200344b350f75e20bb1f7f7c5a131d2e54803
John McNay <john.w.mcnay@gmail.com>
2020-08-01 09:44:38 -0500

    rollDie and twoDice functions created/tested

--- js/array-practice.js
+++ js/array-practice.js
@@ -45,15 +45,33 @@ const removeAll = ( (arr, value) => {
 // console.log(removeAll(bugs, "mosquito"));
 // console.log(removeAll(bugs, "roach"));
 
+//generates a random integer between min (inclusive) and
+//max (exclusive)
 const randomIntBetween = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 // console.log("Random number between 5 and 75:", randomIntBetween(5, 75));
 
+//returns a 0 or 1;
 const coinFlip = () => {
     return randomIntBetween(0, 2);
 }
 
 // console.log(coinFlip());
+
+
+//rolls a die with variable amount of sides and returns the result
+const rollDie = sides => {
+    return randomIntBetween(1, sides+1);
+}
+
+const twoDice = () => {
+    const dieOne = rollDie(6);
+    const dieTwo = rollDie(6);
+
+    return dieOne + dieTwo;
+}
+
+// console.log(twoDice());
 

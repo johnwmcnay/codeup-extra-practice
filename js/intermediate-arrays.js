7095d632a5a60ee5ab7faee0f291f60ba5e7a595
John McNay <john.w.mcnay@gmail.com>
2020-08-01 10:18:48 -0500

    first, secondToLast, rest functions created/tested

--- js/intermediate-arrays.js
+++ js/intermediate-arrays.js
@@ -1,5 +1,24 @@
-
+//returns the first element of an array
 const first = arr => {
     return arr[0];
 }
+
+console.log("first [2, 4, 6]", first([2, 4, 6]));
+console.log("first [5, 1, 3]", first([5, 1, 3]));
+
+//returns the second to last element of an array
+const secondToLast = arr => {
+    return arr[arr.length - 2];
+}
+
+console.log("secondToLast [2, 4, 6]", secondToLast([2, 4, 6]));
+console.log("secondToLast [5, 1, 3]", secondToLast([5, 1, 3]));
+
+//returns all elements of an array after the first element
+const rest = arr => {
+    return arr.slice(1);
+}
+
+console.log("rest [2, 4, 6]", rest([2, 4, 6]));
+console.log("rest [5, 1, 3]", rest([5, 1, 3]));
 

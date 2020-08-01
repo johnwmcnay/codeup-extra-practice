6bfe7c4e4abf1110649ab16a5c836b2cb22420ba
John McNay <john.w.mcnay@gmail.com>
2020-08-01 10:48:28 -0500

    addTwoArrays, getUniqueValues added/tested

--- js/intermediate-arrays.js
+++ js/intermediate-arrays.js
@@ -1,7 +1,7 @@
 //returns the first element of an array
 const first = arr => {
     return arr[0];
-}
+};
 
 // console.log("first [2, 4, 6]", first([2, 4, 6]));
 // console.log("first [5, 1, 3]", first([5, 1, 3]));
@@ -9,7 +9,7 @@ const first = arr => {
 //returns the second to last element of an array
 const secondToLast = arr => {
     return arr[arr.length - 2];
-}
+};
 
 // console.log("secondToLast [2, 4, 6]", secondToLast([2, 4, 6]));
 // console.log("secondToLast [5, 1, 3]", secondToLast([5, 1, 3]));
@@ -17,7 +17,7 @@ const secondToLast = arr => {
 //returns all elements of an array after the first element
 const rest = arr => {
     return arr.slice(1);
-}
+};
 
 // console.log("rest [2, 4, 6]", rest([2, 4, 6]));
 // console.log("rest [5, 1, 3]", rest([5, 1, 3]));
@@ -35,7 +35,7 @@ const getLongestString = arr => {
         }
     });
     return longestStr;
-}
+};
 
 //takes an array and returns the shortest string in it
 //if multiple strings are same length, returns the first occurrence
@@ -50,10 +50,35 @@ const getShortestString = arr => {
         }
     });
     return shortestStr;
-}
+};
 
 const strArray = ["a", "an", "and", "I", "eye"];
 
 // console.log(getLongestString(strArray));
 // console.log(getShortestString(strArray));
+
+//concatenates two arrays
+const addTwoArrays = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+
+const testArray1 = [1, 2, 3];
+const testArray2 = [4, 5, 6];
+
+// console.log(addTwoArrays(testArray1, testArray2));
+
+//return an array with no duplicate values
+const getUniqueValues = arr => {
+    var uniqueArray = [];
+
+    arr.forEach( (element, index) => {
+        if (arr.indexOf(element) === index) {
+            uniqueArray.push(element);
+        }
+    });
+    return uniqueArray;
+};
+
+const duplicateEntries = ["a", "b", "a", "c", "b", "c"];
+// console.log(getUniqueValues(duplicateEntries));
 

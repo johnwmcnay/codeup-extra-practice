cce1426687482ff52b4a4ddcbc06269b2a751eae
John McNay <john.w.mcnay@gmail.com>
2020-08-01 11:10:14 -0500

    removeAll added/tested, moved code into IIFE

--- js/intermediate-arrays.js
+++ js/intermediate-arrays.js
@@ -1,121 +1,136 @@
-//returns the first element of an array
-const first = arr => {
-    return arr[0];
-};
+( () => {
 
-// console.log("first [2, 4, 6]", first([2, 4, 6]));
-// console.log("first [5, 1, 3]", first([5, 1, 3]));
+    //returns the first element of an array
+    const first = arr => {
+        return arr[0];
+    };
 
-//returns the second to last element of an array
-const secondToLast = arr => {
-    return arr[arr.length - 2];
-};
+    // console.log("first [2, 4, 6]", first([2, 4, 6]));
+    // console.log("first [5, 1, 3]", first([5, 1, 3]));
 
-// console.log("secondToLast [2, 4, 6]", secondToLast([2, 4, 6]));
-// console.log("secondToLast [5, 1, 3]", secondToLast([5, 1, 3]));
+    //returns the second to last element of an array
+    const secondToLast = arr => {
+        return arr[arr.length - 2];
+    };
 
-//returns all elements of an array after the first element
-const rest = arr => {
-    return arr.slice(1);
-};
+    // console.log("secondToLast [2, 4, 6]", secondToLast([2, 4, 6]));
+    // console.log("secondToLast [5, 1, 3]", secondToLast([5, 1, 3]));
 
-// console.log("rest [2, 4, 6]", rest([2, 4, 6]));
-// console.log("rest [5, 1, 3]", rest([5, 1, 3]));
+    //returns all elements of an array after the first element
+    const rest = arr => {
+        return arr.slice(1);
+    };
 
-//takes an array and returns the longest string in it
-//if multiple strings are same length, returns the first occurrence
-const getLongestString = arr => {
-    var longestStr = '';
-    var longestLength = 0;
+    // console.log("rest [2, 4, 6]", rest([2, 4, 6]));
+    // console.log("rest [5, 1, 3]", rest([5, 1, 3]));
 
-    arr.forEach( element => {
-        if (element.length > longestLength) {
-            longestStr = element;
-            longestLength = element.length
-        }
-    });
-    return longestStr;
-};
+    //takes an array and returns the longest string in it
+    //if multiple strings are same length, returns the first occurrence
+    const getLongestString = arr => {
+        var longestStr = '';
+        var longestLength = 0;
 
-//takes an array and returns the shortest string in it
-//if multiple strings are same length, returns the first occurrence
-const getShortestString = arr => {
-    var shortestStr = '';
-    var shortestLength = Infinity;
+        arr.forEach( element => {
+            if (element.length > longestLength) {
+                longestStr = element;
+                longestLength = element.length
+            }
+        });
+        return longestStr;
+    };
 
-    arr.forEach( element => {
-        if (element.length < shortestLength) {
-            shortestStr = element;
-            shortestLength = element.length
-        }
-    });
-    return shortestStr;
-};
+    //takes an array and returns the shortest string in it
+    //if multiple strings are same length, returns the first occurrence
+    const getShortestString = arr => {
+        var shortestStr = '';
+        var shortestLength = Infinity;
 
-const strArray = ["a", "an", "and", "I", "eye"];
+        arr.forEach( element => {
+            if (element.length < shortestLength) {
+                shortestStr = element;
+                shortestLength = element.length
+            }
+        });
+        return shortestStr;
+    };
 
-// console.log(getLongestString(strArray));
-// console.log(getShortestString(strArray));
+    const strArray = ["a", "an", "and", "I", "eye"];
 
-//concatenates two arrays
-const addTwoArrays = (arr1, arr2) => {
-    return arr1.concat(arr2);
-};
+    // console.log(getLongestString(strArray));
+    // console.log(getShortestString(strArray));
 
-const testArray1 = [1, 2, 3];
-const testArray2 = [4, 5, 6];
+    //concatenates two arrays
+    const addTwoArrays = (arr1, arr2) => {
+        return arr1.concat(arr2);
+    };
 
-// console.log(addTwoArrays(testArray1, testArray2));
+    const testArray1 = [1, 2, 3];
+    const testArray2 = [4, 5, 6];
 
-//return an array with no duplicate values
-const getUniqueValues = arr => {
-    var uniqueArray = [];
+    // console.log(addTwoArrays(testArray1, testArray2));
 
-    arr.forEach( (element, index) => {
-        if (arr.indexOf(element) === index) {
-            uniqueArray.push(element);
-        }
-    });
-    return uniqueArray;
-};
+    //return an array with no duplicate values
+    const getUniqueValues = arr => {
+        var uniqueArray = [];
 
-const duplicateEntries = ["a", "b", "a", "c", "b", "c"];
-// console.log(getUniqueValues(duplicateEntries));
+        arr.forEach( (element, index) => {
+            if (arr.indexOf(element) === index) {
+                uniqueArray.push(element);
+            }
+        });
+        return uniqueArray;
+    };
 
-//reverses the array
-const reverseArray = arr => {
-    return arr.reverse();
-};
+    const duplicateEntries = ["a", "b", "a", "c", "b", "c"];
+    // console.log(getUniqueValues(duplicateEntries));
 
-// console.log(reverseArray(testArray1));
-// console.log(reverseArray(testArray2));
+    //reverses the array
+    const reverseArray = arr => {
+        return arr.reverse();
+    };
 
-const quotes = ["All the world's a stage...",
-    "Give me liberty or give me death",
-    "An eye for an eye leaves the whole world blind",
-    ]
+    // console.log(reverseArray(testArray1));
+    // console.log(reverseArray(testArray2));
 
-//gets a random quote/index from an optional array argument
-const getRandomQuote = (arr=quotes) => {
-    var index = Math.floor(Math.random() * arr.length);
-    return arr[index];
-};
+    const quotes = ["All the world's a stage...",
+        "Give me liberty or give me death",
+        "An eye for an eye leaves the whole world blind",
+        ]
 
-// console.log(getRandomQuote());
-// console.log(getRandomQuote(duplicateEntries));
+    //gets a random quote/index from an optional array argument
+    const getRandomQuote = (arr=quotes) => {
+        var index = Math.floor(Math.random() * arr.length);
+        return arr[index];
+    };
 
-const getIndexesOf = (char, word) => {
-    const strArray = word.split('');
-    var newArray = [];
+    // console.log(getRandomQuote());
+    // console.log(getRandomQuote(duplicateEntries));
 
-    strArray.forEach( (letter, index) => {
-        if (letter === char) {
-            newArray.push(index);
-        }
-    });
-    return newArray;
-};
+    const getIndexesOf = (char, word) => {
+        const strArray = word.split('');
+        var newArray = [];
 
-// console.log(getIndexesOf("a", "banana"));
-// console.log(getIndexesOf("z", "banana"));
+        strArray.forEach( (letter, index) => {
+            if (letter === char) {
+                newArray.push(index);
+            }
+        });
+        return newArray;
+    };
 
+    // console.log(getIndexesOf("a", "banana"));
+    // console.log(getIndexesOf("z", "banana"));
+
+    //returns an array with all elements equal to value removed
+    const removeAll = (arr, value) => {
+        return arr.filter( (element) => {
+            return element !== value;
+        });
+    };
+
+    // console.log(removeAll([1,2,3], 2));
+    // console.log(removeAll([2,2,3,4,5,2,2], 2));
+
+
+
+})();
